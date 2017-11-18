fd074407952cc92b7516ac1d25180e9d7e4270d1
quentin <trunk@DESKTOP-E2RE1IE>
2017-11-18 15:51:04 +0100

    Map All Fonctionnalities

--- Snapchat/app/src/main/java/com/littlebiig/snapchat/map_displayPicture.java
+++ Snapchat/app/src/main/java/com/littlebiig/snapchat/map_displayPicture.java
@@ -1,9 +1,17 @@
 package com.littlebiig.snapchat;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.app.Activity;
+import android.support.annotation.NonNull;
 import android.widget.ImageView;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 //import com.tbuonomo.jawgmapsample.R;
 
@@ -16,11 +24,35 @@ public class map_displayPicture extends Activity {
         String id = intent.getStringExtra("id");
 
 
+
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+        StorageReference storageRef = storage.getReference();
+        String fileName = id+".jpg";
+        StorageReference imagesRef = storageRef.child(fileName);
+
+
+        final long ONE_MEGABYTE = 1024 * 1024;
+        imagesRef.getBytes(ONE_MEGABYTE).addOnSuccessListener(new OnSuccessListener<byte[]>() {
+            @Override
+            public void onSuccess(byte[] bytes) {
+                // Data for "images/island.jpg" is returns, use this as needed
+                Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+                setContentView(R.layout.map_display_picture);
+                ImageView img= (ImageView) findViewById(R.id.imageView);
+                img.setImageBitmap(bitmap);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                // Handle any errors
+            }
+        });
+
+
+
+        //setContentView(R.layout.map_display_picture);
+        //ImageView img= (ImageView) findViewById(R.id.imageView);
         //img.setImageResource(getResources().getIdentifier(id, "drawable", getPackageName()));
-        setContentView(R.layout.map_display_picture);
-        ImageView img= (ImageView) findViewById(R.id.imageView);
-        //img.setImageResource(R.drawable.image);
-        img.setImageResource(getResources().getIdentifier(id, "drawable", getPackageName()));
     }
 
 }
